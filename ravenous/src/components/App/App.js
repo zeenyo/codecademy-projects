880bad0c860fa2a1f36de1441555d3809992e0cd
zeenyo <zeno.poopito@gmail.com>
2022-01-04 06:01:12 -0500

    end ravenous 3

---
+++ ravenous/src/components/App/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+
+
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
+
+class App extends React.Component {
+  searchYelp(term, location, sortBy){
+      console.log(`Searching for ${term} near ${location} by ${sortBy}.`);
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={businesses}/>
+      </div>
+    );
+  }
+}
+
+export default App;
