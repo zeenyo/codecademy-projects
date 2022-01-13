dc551389fc1e6dd6a93cb7326537e84b695ec808
zeenyo <93214746+zeenyo@users.noreply.github.com>
2022-01-13 00:15:27 -0500

    Create wanderlust.js
    completed Wanderlust 31/32, foursquare api v3 update

---
+++ wanderlust.js
@@ -0,0 +1,84 @@
+// Foursquare API Info
+const foursquareKey = '';
+const url = 'https://api.foursquare.com/v3/places/search?near=';
+
+// OpenWeather Info
+const openWeatherKey = '';
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+// Page Elements
+const $input = $('#city');
+const $submit = $('#button');
+const $destination = $('#destination');
+const $container = $('.container');
+const $placeDivs = [$("#place1"), $("#place2"), $("#place3"), $("#place4")];
+const $weatherDiv = $("#weather1");
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+const options = {
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+    Authorization: foursquareKey
+  }
+};
+
+// Add AJAX functions here:
+const getPlaces = async () => {
+  const city = $input.val();
+  const urlToFetch = `${url}${city}&limit=10`;
+  try {
+    const response = await fetch(urlToFetch, options);
+    if(response.ok){
+      const jsonResponse = await response.json();
+      const places = jsonResponse.results;
+      return places;
+    }
+  } catch(error) {
+    console.log(`THIS IS THE ERROR !!!!!! ${error}`);
+  }
+};
+
+const getForecast = async () => {
+  const urlToFetch = `${weatherUrl}?&q=${$input.val()}&APPID=${openWeatherKey}`;
+  try {
+    const response = await fetch(urlToFetch);
+    if(response.ok){
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+  } catch(error) {
+    console.log(`THIS IS THE ERROR !!!!!! ${error}`);
+  }
+};
+
+
+// Render functions
+const renderPlaces = (places) => {
+  $placeDivs.forEach(($place, index) => {
+    // Add your code here:
+    const place = places[index];
+    const placeIcon = place.categories[0].icon;
+    const placeImgSrc = `${placeIcon.prefix}bg_64${placeIcon.suffix}`;
+    const placeContent = createPlaceHTML(place.name, place.location, placeImgSrc);
+    $place.append(placeContent);
+  });
+  $destination.append(`<h2>${places[0].location.locality}</h2>`);
+};
+
+const renderForecast = (forecast) => {
+  const weatherContent = createWeatherHTML(forecast);
+  $weatherDiv.append(weatherContent);
+};
+
+const executeSearch = () => {
+  $placeDivs.forEach(place => place.empty());
+  $weatherDiv.empty();
+  $destination.empty();
+  $container.css("visibility", "visible");
+  getPlaces().then(places => renderPlaces(places));
+  getForecast().then(forecast => renderForecast(forecast));
+  return false;
+}
+
+$submit.click(executeSearch);
